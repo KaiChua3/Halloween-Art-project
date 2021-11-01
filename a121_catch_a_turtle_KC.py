f0b4b8d59454723f9d5768b21368f1055328f681
KaiChua3 <91628674+KaiChua3@users.noreply.github.com>
2021-11-01 11:05:37 -0700

    Update a121_catch_a_turtle_KC.py

--- a121_catch_a_turtle_KC.py
+++ a121_catch_a_turtle_KC.py
@@ -7,7 +7,8 @@ import random as rand
 fill_color = "blue"
 shape_size = 3
 shape = "triangle"
-
+list = ["Halloween is on October 31st","Candy corn used to be called Chicken feed","Only 65% of Americans celebrate halloween","Jack'o lanterns in Ireland and Scotland was made out of turnips, beets, and potatoes instead of pumpkins",""]
+#'halloween-2019-blog-1-1.gif', 'download.gif', 'halloween-gettyimages-172988453.gif'
 #-----initialize turtle-----
 painter = trtl.Turtle()
 painter.shape("triangle")
@@ -17,17 +18,16 @@ painter.fillcolor("blue")
 #-----game functions--------
 def painter_clicked(x,y):
     change_position()
+    halloween_picture(x)
 def change_position():
     new_xpos = rand.randint(-400,400)
     new_ypos = rand.randint(-300,300)
     painter.penup() 
     painter.goto(new_xpos,new_ypos)
-def halloween_picture():
-    picture = rand.choice(0,2)
+def halloween_picture(x):
+    x = rand.randint(0,2)
+    print(list[x])
 #-----events----------------
 wn = trtl.Screen()
-wn.addshape('halloween-2019-blog-1-1.gif')
-wn.addshape('download.gif')
-wn.addshape('halloween-gettyimages-172988453.gif')
 painter.onclick(painter_clicked)
 wn.mainloop()
